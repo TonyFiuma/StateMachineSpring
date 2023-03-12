95ee8b6cfe587057e6bf3896a7f370d59d5999d8
Antonio Fiumanò <113920158+TonyFiuma@users.noreply.github.com>
2023-03-12 19:52:58 +0100

    Add files via upload

---
+++ StateMachineSpringBoot/src/main/java/co/develhope/loginDemo/order/entities/OrderStateEnum.java
@@ -0,0 +1,11 @@
+package co.develhope.loginDemo.order.entities;
+
+public enum OrderStateEnum{
+    CREATED,
+    ACCEPTED,
+    IN_PREPARATION,
+    READY,
+    DELIVERING,
+    COMPLETED,
+    CLOSED
+}
---
+++ StateMachineSpringBoot/src/main/java/co/develhope/loginDemo/salary/entities/CreateSalaryDTO.java
@@ -0,0 +1,22 @@
+package co.develhope.loginDemo.salary.entities;
+
+
+public class CreateSalaryDTO {
+
+    long amount;
+
+    public CreateSalaryDTO(){
+    }
+
+    public CreateSalaryDTO(long amount){
+        this.amount = amount;
+    }
+
+    public long getAmount(){
+        return amount;
+    }
+
+    public void setAmount(long amount){
+        this.amount = amount;
+    }
+}
